14e352bb08126fdb7443a870a2b49d36e7a50af3
Vaibhav <vaibhav@bluesapling.com>
2020-11-03 18:51:03 +0530

    Finish 5.3

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,17 @@ import blogService from './services/blogs';
 import loginService from './services/login';
 
 const App = () => {
+  const defaultFormState = () => ({
+    title: '',
+    author: '',
+    url: '',
+  });
+
   const [blogs, setBlogs] = useState([]);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [user, setUser] = useState(null);
+  const [formState, setFormState] = useState(defaultFormState());
 
   useEffect(() => {
     blogService.getAll().then((blogsData) => setBlogs(blogsData));
@@ -18,7 +25,7 @@ const App = () => {
     if (loggedNoteappUser) {
       const userFromLocalStorage = JSON.parse(loggedNoteappUser);
       setUser(userFromLocalStorage);
-      blogService.setToken(userFromLocalStorage);
+      blogService.setToken(userFromLocalStorage.token);
     }
   }, []);
 
@@ -37,13 +44,25 @@ const App = () => {
     } catch (error) {
       throw new Error(error);
     }
-
-    console.log('logging in with', username, password);
   };
 
   const handleLogout = () => {
     window.localStorage.clear();
     setUser(null);
+  };
+
+  const handleBlogCreation = (e) => {
+    e.preventDefault();
+    blogService.create(formState)
+      .then(() => {
+        blogService.getAll().then((blogsData) => setBlogs(blogsData));
+      });
+  };
+
+  const handleFormFieldChange = (name, value) => {
+    const formStateCopy = { ...formState };
+    formStateCopy[name] = value;
+    setFormState(formStateCopy);
   };
 
   const loginForm = () => (
@@ -76,6 +95,45 @@ const App = () => {
     </>
   );
 
+  const newBlogForm = () => (
+    <>
+      <form onSubmit={handleBlogCreation}>
+        <label htmlFor="title">
+          title:
+          <input
+            id="title"
+            name="title"
+            required
+            value={formState.title}
+            onChange={(e) => handleFormFieldChange('title', e.target.value)}
+          />
+        </label>
+        <label htmlFor="author">
+          author:
+          <input
+            id="author"
+            name="author"
+            required
+            value={formState.author}
+            onChange={(e) => handleFormFieldChange('author', e.target.value)}
+          />
+        </label>
+        <label htmlFor="url">
+          url:
+          <input
+            id="url"
+            name="url"
+            required
+            value={formState.url}
+            onChange={(e) => handleFormFieldChange('url', e.target.value)}
+          />
+        </label>
+
+        <button type="submit">Save</button>
+      </form>
+    </>
+  );
+
   const isUserLoggedIn = !!(user);
 
   return (
@@ -90,6 +148,7 @@ const App = () => {
             {`${user.name || user.username} is now logged in.`}
             <button onClick={handleLogout} type="button">Logout</button>
           </p>
+          {newBlogForm()}
           {blogsUI()}
         </>
       )}
