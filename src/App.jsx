043df4e99264c49090cadfa0cca12d616c7d675b
Vaibhav <vaibhav@bluesapling.com>
2020-11-12 19:08:42 +0530

    Finish 5.9

--- src/App.jsx
+++ src/App.jsx
@@ -95,7 +95,9 @@ const App = () => {
 
   const blogsUI = () => (
     <>
-      {blogs.map((blog) => <Blog key={blog.id} blog={blog} />)}
+      {blogs
+        .sort((blog1, blog2) => blog1.likes < blog2.likes)
+        .map((blog) => <Blog key={blog.id} blog={blog} />)}
     </>
   );
 
